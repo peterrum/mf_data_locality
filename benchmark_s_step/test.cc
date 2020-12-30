b007d895bb23068cf822ef9c819409db92f4c189
Peter Munch <peterrmuench@gmail.com>
2020-12-30 14:49:41 +0100

    Template SolverCGSStep

--- benchmark_s_step/test.cc
+++ benchmark_s_step/test.cc
@@ -14,11 +14,20 @@ run_cg_solver(const Operator &                                  laplace_operator
   (void)preconditioner;
 
   ReductionControl solver_control(100 / n_steps, 1e-15, 1e-8);
-  SolverCGSStep    solver(solver_control, n_steps);
 
   try
     {
-      solver.solve(laplace_operator, x, b);
+      if (n_steps == 1)
+        SolverCGSStep<1>(solver_control).solve(laplace_operator, x, b);
+      else if (n_steps == 2)
+        SolverCGSStep<2>(solver_control).solve(laplace_operator, x, b);
+      else if (n_steps == 4)
+        SolverCGSStep<4>(solver_control).solve(laplace_operator, x, b);
+      else if (n_steps == 6)
+        SolverCGSStep<6>(solver_control).solve(laplace_operator, x, b);
+      else
+        Assert(false, ExcNotImplemented());
+
       return solver_control.last_step();
     }
   catch (SolverControl::NoConvergence &e)
